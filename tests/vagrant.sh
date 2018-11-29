8a95d4e56045f466821c330dc8355484932251aa
Mischa ter Smitten <mischa@tersmitten.nl>
2018-11-29 22:52:28 +0100

    Fix Ansible warnings Add support for Ubuntu 18.04 and Debian 9

--- tests/vagrant.sh
+++ tests/vagrant.sh
@@ -8,7 +8,7 @@ thisFile="$(readlink -f ${0})";
 thisFilePath="$(dirname ${thisFile})";
 
 # Only provision once
-if [ -f /provisioned ]; then
+if [[ -f /provisioned ]]; then
   exit 0;
 fi
 
@@ -18,11 +18,15 @@ shopt -s expand_aliases;
 alias apt-update='apt-get update -qq';
 alias apt-install='apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"';
 
-# No PHP 5 support in 16.04
-if $(lsb_release -r | grep -q '16.04'); then
-  PHP_VERSION='7.0';
-else
-  PHP_VERSION='5';
+distributor_id="$(lsb_release -i | awk '{print $3}' | tr '[:upper:]' '[:lower:]')";
+distributor_version="$(lsb_release -r | awk '{print $2}')";
+major_version="$(echo ${distributor_version} | awk -F. '{print $1}')";
+
+PHP_VERSION='';
+if [ "${distributor_id}" = 'ubuntu' ] && [ "${major_version}" -le "14" ]; then
+    PHP_VERSION='5';
+elif [ "${distributor_id}" = 'debian' ] && [ "${major_version}" -le "8" ]; then
+    PHP_VERSION='5';
 fi
 apt-update && apt-install "php${PHP_VERSION}-cli" curl;
 
